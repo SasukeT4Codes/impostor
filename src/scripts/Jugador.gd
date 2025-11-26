dd968c319b4574c8ce90e2a214c64ec657cf9c5b
SasukeT4Codes <st4codes@gmail.com>
2025-11-26 00:54:48 -0500

    pseudofuncional

--- src/scripts/Jugador.gd
+++ src/scripts/Jugador.gd
@@ -9,20 +9,28 @@ func _ready():
 	avatar_btn.pressed.connect(_on_avatar_pressed)
 	perfil_list.item_selected.connect(_on_perfil_selected)
 
+	# Cargar íconos automáticamente desde assets
+	for i in range(1, 8):
+		var path := "res://src/assets/characters/char-%02d.png" % i
+		var icon: Texture = load(path)
+		if icon:
+			perfil_list.add_item("", icon)
+
 func _on_avatar_pressed():
 	perfil_list.visible = not perfil_list.visible
 
-func _on_perfil_selected(index:int):
-	# Aquí luego asignas la textura real del perfil
-	avatar_btn.text = "P%d" % (index + 1)
+func _on_perfil_selected(index: int):
+	var icon: Texture = perfil_list.get_item_icon(index)
+	if icon:
+		avatar_btn.texture_normal = icon
 	perfil_list.visible = false
 
-func set_default_name(id:int):
+func set_default_name(id: int):
 	nombre_edit.text = "Jugador%d" % (id + 1)
 
 func get_data() -> Dictionary:
 	return {
 		"id": get_index(),
 		"nombre": nombre_edit.text,
-		"perfil": avatar_btn.text
+		"imagen": avatar_btn.texture_normal
 	}
